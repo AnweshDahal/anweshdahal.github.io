bb1de1788f0570c7663309b5fae97576261c0365
AnweshDahal <anweshd2000@gmail.com>
2021-01-26 15:22:59 +0545

    Adds go up button

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-// Script File 
+// Script File
+var goUpButton = document.getElementById("goUp");
+window.onscroll = function () {
+	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		goUpButton.style.opacity = 1;
+		goUpButton.style.cursor = "pointer";
+	} else {
+		goUpButton.style.opacity = 0;
+		goUpButton.style.cursor = "default";
+	}
+};
